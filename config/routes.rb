e7ca99311f4469df8ef5304ee7bbc85f5ef19297
easbarba <easbarba@outlook.com>
2022-05-10 22:39:28 -0300

    posts: add initial files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'posts#index'
+
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/destroy'
+  get 'posts/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
