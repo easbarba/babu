b59579ddb86f0f156898c866560ff1c956153e14
easbarba <easbarba@outlook.com>
2022-05-10 22:42:23 -0300

    routes/posts: use only feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/destroy'
-  get 'posts/update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts, only: %i[index new create destroy update]
 end
