6d916270aa4cad79dd302896e13f977016ae9376
easbarba <easbarba@outlook.com>
2022-05-12 11:58:47 -0300

    devise: add user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # , :lockable, :timeoutable, and :omniauthable
-  devise :confirmable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
