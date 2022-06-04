653860ad0194d6872e92ca69925330cc93473d7a
easbarba <easbarba@outlook.com>
2022-06-04 00:12:24 -0300

    improve theme on registration and login pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,18 @@
 class User < ApplicationRecord
-  before_save {
+  before_save do
     self.email = email.downcase
-  }
+  end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
   has_many :posts, dependent: :destroy
   has_one :profile
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-                    uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
 end
