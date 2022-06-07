c462e37c7644c0b24b19b66907e140bff9d98e59
easbarba <easbarba@outlook.com>
2022-06-07 13:46:28 -0300

    cfg/routes: rm unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'profiles/index'
   root to: 'welcome#index'
-  get 'welcome/index'
+
   get 'profile/index'
 
   resources :posts
   resources :profiles
+
   devise_for :users
 end
