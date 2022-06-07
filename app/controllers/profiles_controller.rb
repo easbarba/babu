e3a051aedee96bed36437a97e5e94ed1f2515920
easbarba <easbarba@outlook.com>
2022-06-07 13:47:12 -0300

    profiles: let user to edit biography

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,38 @@
 class ProfilesController < ApplicationController
-  before_action :set_post, only: %i[show]
+  before_action :set_profile, only: %i[show edit update]
 
-  def index
-    @profiles = Post.all
-  end
+  # def index
+  #   @profiles = Profile.all
+  # end
 
   def show; end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @profile.update(profile_params)
+        format.html do
+          redirect_to profile_url(@profile), notice: 'Perfil atualizado com sucesso.'
+        end
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_post
+  def set_profile
+    current_profile = Profile.find(params[:id])
+
+    @all_permissions = user_signed_in? && current_user.id == current_profile.id
     @profile = Profile.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def profile_params
+    params.require(:profile).permit(:bio)
   end
 end
