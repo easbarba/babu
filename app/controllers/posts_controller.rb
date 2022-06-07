35468d0c55c74b73e0bf85b8ffd9f846e5ef9801
easbarba <easbarba@outlook.com>
2022-06-07 13:58:23 -0300

    posts: translate notifications

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
                           .page(current_page).per 5
   end
 
-  # GET /posts/1 or /posts/1.json
+  # GET /posts/1
   def show; end
 
   # GET /posts/new
@@ -23,41 +23,36 @@ class PostsController < ApplicationController
   # GET /posts/1/edit
   def edit; end
 
-  # POST /posts or /posts.json
+  # POST /posts
   def create
     @post = current_user.posts.build post_params
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        format.html { redirect_to post_url(@post), notice: 'Nova estória criada.' }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
+  # PATCH/PUT /posts/1
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+        format.html { redirect_to post_url(@post), notice: 'Estória corrigida.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
+  # DELETE /posts/1
   def destroy
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to posts_url, notice: 'Estória agora esquecida.' }
     end
   end
 
