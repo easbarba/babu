c7f4d198ae7031126f6c765bd1ed9fa638506ab7
easbarba <easbarba@outlook.com>
2022-06-09 12:05:59 -0300

    profiles: list user stories

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,7 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: %i[show edit update]
+  before_action :set_posts, only: %i[show]
+  before_action :set_permission, only: %i[show]
 
   def show; end
 
@@ -19,13 +21,21 @@ class ProfilesController < ApplicationController
 
   private
 
+  def set_posts
+    current_page = (params[:page] || 0).to_i
+
+    @posts = @profile.user.posts.order(created_at: :desc)
+                     .page(current_page).per 4
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_profile
     @profile = Profile.find params[:id]
-    @posts = current_user.posts
+  end
 
+  def set_permission
     # user found and is the correct user to edit current profile.
-    @all_permissions = user_signed_in? && current_user.id == @profile.id
+    @all_permissions = user_signed_in? && current_user.profile.id == @profile.id
   end
 
   # Only allow a list of trusted parameters through.
